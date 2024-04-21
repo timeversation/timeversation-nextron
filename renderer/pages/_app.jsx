80365f5e6a3cd4ed9a6891bd75d4ba0c73d7dda7
wonglok <yellowhappy831@gmail.com>
2024-04-22 07:37:02 +0800

    fa

--- renderer/pages/_app.jsx
+++ renderer/pages/_app.jsx
@@ -1,8 +1,12 @@
+import { useEffect } from 'react'
 import { Popup, Overlay } from '../components/UI/Overlay/Overlay'
 import '../components/global.css'
 
 export default function App({ Component, pageProps }) {
 
+    useEffect(() => {
+        // launcher-urls
+    }, [])
 
     return <>
         <Component {...pageProps} />
